5c4d2b1ee3e60509b54cbf90b7f6eec9c399ee09
Edwin Lee <leeed2001@gmail.com>
2016-05-28 09:08:37 -0600

    Update webpage

--- bring_over_html.sh
+++ bring_over_html.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-rm -rf _sources/ _static/
+rm -rf _sources/ _static/ api/
 rm -f *.html *.js *.inv
 cp -r ../Tpe2Bcl/docs/_build/html/* .
+mkdir api
+cp -r ../Tpe2Bcl/tpe-to-bcl/doc/* api/ 
 git add *
 git commit -am 'Update webpage'
 git push mygithub gh-pages
